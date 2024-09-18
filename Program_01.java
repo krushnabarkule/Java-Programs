fd84d615bb0927c092f3ff9f250c374a23026542
krushnabarkule <85426686+krushnabarkule@users.noreply.github.com>
2024-09-18 19:42:20 +0530

    First commit

---
+++ Program_01.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Program_01 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java,\nLearning Java Now and\nProgramming is Fun");
+    }
+}
